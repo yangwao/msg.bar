16ac77c5f2ec725996534f087f40b436853b720f
Matej Nemcek <ybdaba@gmail.com>
2017-09-02 17:20:00 +0200

    new vue pwa template

---
+++ msgbar/src/App.vue
@@ -0,0 +1,74 @@
+<template>
+<div id="main">
+      <div class="columns">
+            <div class="column">
+                  <div class="crop">
+                        <a href="https://progressbar.sk">
+                              <img src="./assets/progressbar-logo.svg" alt="progressbar logo">
+                        </a>
+                  </div>
+
+            </div>
+      </div>
+      <div class="columns is-mobile">
+            <form action="http://msg.bar" method="POST">
+                  <div class="column">
+                        <input class="button is-primary is-outlined is-large" type="submit" value="Display" />
+                        <a href="https://donate.progressbar.sk">
+                              <button class="button is-warning is-outlined is-large">Send 💰⛓ donation</button>
+                        </a>
+                  </div>
+                  <div class="column">
+                        <div class="field">
+                              <label class="label"></label>
+                              <div class="control">
+                                    <textarea autofocus name="msg" rows="5" class="textarea" placeholder="🤘 type
+🛰 something
+🌐 here
+🔐 hit
+📡 display" maxlength="72">
+                                    </textarea>
+                              </div>
+                        </div>
+
+                  </div>
+            </form>
+      </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+@import "~bulma/css/bulma.css";
+
+html {
+    background-color: #000
+}
+
+body {}
+
+.textarea {
+    border-color: whitesmoke;
+    color: whitesmoke;
+    background-color: black;
+    font-size: 32px;
+}
+
+.crop {
+    /*width: 200px;*/
+    height: 150px;
+    overflow: hidden;
+    padding: 10px;
+}
+
+.crop img {
+    /*width: 400px;*/
+    /*height: 300px;*/
+    margin: -128px 0 0 -10px;
+}
+</style>
---
+++ msgbar/src/components/Hello.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="columns">
+    <div class="column">
+      First column
+    </div>
+    <div class="column">
+      Second column
+    </div>
+    <div class="column">
+      Third column
+    </div>
+    <div class="column">
+      Fourth column
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js PWA'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+@import "~bulma/css/bulma.css";
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #35495E;
+}
+</style>
